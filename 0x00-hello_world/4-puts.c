fa79bbbb8d2ccac67b0f879d4e47cc7ab3b80713
komodoyakuzan <sakwamalcolm@gmail.com>
2024-06-20 13:39:51 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * *main - this is the main funtion
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle);
 	return (0);
 }
